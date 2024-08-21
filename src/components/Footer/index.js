db604ad45e3fce44639583e61eca23e3c74185e1
Matheus Artur <matheusartur1@hotmail.com>
2024-08-20 21:50:48 -0300

    Criando a plataforma TechShelf

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer () {
+  return(
+    <footer className={styles.footer}>
+      <h2>TechShelf &copy; Desenvolvido por Matheus Artur 2024</h2>
+    </footer>
+  );
+}
+
+export default Footer;
